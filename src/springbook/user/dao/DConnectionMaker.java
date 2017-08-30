e7098984682c71bd8580d4bc327d2926c31b1a50
Administrator <Administrator@bwkwon-PC>
2017-08-30 14:47:09 +0900

    클래스 대신 인터페이스를 사용해서 DB커넥션을 가져오도록 수정

--- src/springbook/user/dao/DConnectionMaker.java
+++ src/springbook/user/dao/DConnectionMaker.java
@@ -4,9 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class SimpleConnectionMaker {
-	
-	public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+public class DConnectionMaker implements ConnectionMaker {
+
+	public Connection makeConnection() throws ClassNotFoundException, SQLException {
+		
+		// D사의 독자적인 방법으로 Connection을 생성하는 코드
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
 		
