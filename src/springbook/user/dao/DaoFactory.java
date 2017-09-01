8875901afdf4d7551fa1d0d23e2a8d328d204aa7
Administrator <Administrator@bwkwon-PC>
2017-09-01 13:40:50 +0900

    팩토리 수정

--- src/springbook/user/dao/DaoFactory.java
+++ src/springbook/user/dao/DaoFactory.java
@@ -3,10 +3,7 @@ package springbook.user.dao;
 public class DaoFactory {
 	
 	public UserDao userDao() {
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao userDao = new UserDao(connectionMaker);
-		
-		return userDao;
+		return new UserDao(new DConnectionMaker());
 	}
 
 }
